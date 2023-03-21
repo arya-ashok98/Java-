b2bc6549adcfd64b01f7c1c6ab6d31c1985d084b
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-21 22:48:37 +0530

    Add files via upload

---
+++ DisplayUserInput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DisplayUserInput {
+
+     public static void main(String args[]) {
+ 
+           int input;
+
+           Scanner sc = new Scanner(System.in);
+
+           System.out.println("Enter the integer input: ");
+
+           input = sc.nextInt();  // getting/reading an integer input from the user and storing in the variable 'input'
+
+           System.out.println("The input entered by the user is : " +input);
+
+
+     }
+
+}
+
+           
+  
+           
