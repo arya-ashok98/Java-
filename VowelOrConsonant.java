1dc095ee8d9dd23593898a1e5a70ac39315d0c00
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-22 22:12:24 +0530

    Add files via upload

---
+++ VowelOrConsonant.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+
+   public static void main(String args[]) {
+
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter the alphabet to be checked: ");
+
+      char alphabet = sc.next().charAt(0);
+
+      switch(alphabet) {
+
+   		case 'a': 
+			
+			System.out.println("\nThe given alphabet "+alphabet+" is is a vowel");
+                  break;
+
+		case 'e':
+
+			System.out.println("\nThe given alphabet "+alphabet+" is is a vowel");
+                  break;
+
+		case 'i':
+
+			System.out.println("\nThe given alphabet "+alphabet+" is is a vowel");
+                  break;
+
+		case 'o':
+
+			System.out.println("\nThe given alphabet "+alphabet+" is is a vowel");
+                  break;
+
+		case 'u':
+
+			System.out.println("\nThe given alphabet "+alphabet+" is is a vowel");
+                  break;
+
+            default:
+
+			System.out.println("\nThe given alphabet "+alphabet+" is a consonant");
+
+      }
+
+  }
+
+}
