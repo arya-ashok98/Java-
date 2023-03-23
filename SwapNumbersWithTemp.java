9f5977af9e522f40ce4b0f1402667b3400f99a4d
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-23 12:35:36 +0530

    Add files via upload

---
+++ SwapNumbersWithTemp.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SwapNumbersWithTemp {
+
+    public static void main(String args[]) {
+
+        int temp = 0, numberOne, numberTwo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nEnter the first number: ");
+
+        numberOne = sc.nextInt();
+
+	  System.out.println("\nEnter the second number: ");
+
+        numberTwo = sc.nextInt();
+
+        System.out.println("\nThe value of NumberOne and NumberTwo before swapping is: "+numberOne+" and "+numberTwo);
+
+        temp = numberOne;
+
+        numberOne = numberTwo;
+
+        numberTwo = temp;
+
+        System.out.println("\nThe value of NumberOne and NumberTwo after swapping is: "+numberOne+" and "+numberTwo);
+
+    }
+
+}
+
+        
