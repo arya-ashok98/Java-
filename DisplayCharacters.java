ebb1cbc0ed6085285a9ac61b947dc827f6572384
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-24 15:29:44 +0530

    Add files via upload

---
+++ DisplayCharacters.java
@@ -0,0 +1,21 @@
+public class DisplayCharacters {
+
+	public static void main(String[] args) {
+
+            char i, characters;
+
+            characters = 'A';
+
+            System.out.println("\nThe characters from A to Z: ");
+
+            for(i = 'A'; i <= 'Z'; i++) {
+
+			System.out.println(i);
+
+            }
+		
+	}
+
+}
+
+          
