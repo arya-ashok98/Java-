3a17d951ce33d36f45f8a4385c9578e0592926db
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-24 18:53:42 +0530

    Add files via upload

---
+++ CountDigitsInNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CountDigitsInNumber {
+
+     public static void main(String args[]) {
+
+         int count = 0, number, m;
+
+         Scanner sc = new Scanner(System.in);
+
+         System.out.println("\nEnter the number: ");
+
+         number = sc.nextInt();
+
+         while(number > 0) {
+
+		m = number % 10;
+
+            number = number / 10;
+
+            count++;
+
+         }
+	    
+	   System.out.println("\nThe number is a "+count+" digit number");
+
+     }  
+
+} 
+
+         
