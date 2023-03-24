26be4673344cf29404d9772d64fad8390a34ea78
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-24 14:50:53 +0530

    Add files via upload

---
+++ Fibonocci.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Fibonocci {
+
+    public static void main(String args[]) {
+
+    int a = 0, b = 1, c = 0, range, i;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("\nEnter the range of the series: ");
+
+    range = input.nextInt();
+
+    for(i = 0; i <= range; i++) {
+
+        System.out.println("\n"+a);
+
+	  c = a + b;
+
+        a = b;
+
+        b = c;
+
+        }
+
+    }
+
+}
+
