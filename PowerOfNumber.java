6e52abfcdf39ed4d6ba07b0b5385b58a5aba53fc
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-25 09:51:35 +0530

    Add files via upload

---
+++ PowerOfNumber.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PowerOfNumber {
+
+   public static void main(String[] args) {
+
+	int base, power, m;
+
+	Scanner sc = new Scanner(System.in);
+
+      System.out.println("\nEnter the base: ");
+
+      base = sc.nextInt();
+
+      System.out.println("\nEnter the power: ");
+
+      power = sc.nextInt();
+  
+	m = base; // now the base/number is safe in variable m
+
+      for(int i = 1; i < power; i++) {
+
+           m = m * base;
+	}
+
+	System.out.println("\nThe value for the number "+base+" ^ "+power+" equals to: "+m);
+
+   }
+
+}
