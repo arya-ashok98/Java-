b1f04152ae2208ed66778b8d3463f0a63e12b2b2
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-28 23:29:03 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,38 +1,29 @@
 import java.lang.Math;
 
-public class Triangle {
+public class TriangleWithNoParamConstructor {
 
-	int side1 = 3, side2 = 4, side3 = 5, perimeter;
+	static int side1 = 3, side2 = 4, side3 = 5, perimeter;
 
-      	double area = 0.00, semiPer = 0.00;
+      	static double area = 0.00, semiPer = 0.00;
 
-      	public void calculatePerimeter() {
+      	public TriangleWithNoParamConstructor() {
 
 		perimeter = side1 + side2 + side3;
-
-	      	System.out.println("\nThe perimeter of the triangle is: "+perimeter+" units");
-
-	}
-
-	public void calculateArea() {
 
 		semiPer = perimeter/2;     //(side1 + side2 + side3)/2;
 
 		area = Math.sqrt(semiPer * (semiPer - side1) * (semiPer - side2) * (semiPer - side3));
-
-		System.out.println("\nThe area of the triangle is: "+area+" sq units");
 
 	}	
 
 
 	public static void main(String args[]) {
 
-		Triangle triangle = new Triangle();
+		TriangleWithNoParamConstructor triangle = new TriangleWithNoParamConstructor();
 
-		triangle.calculatePerimeter();
+		System.out.println("\nThe perimeter of the triangle is: "+perimeter+" units");
 
-		triangle.calculateArea();
-
+		System.out.println("\nThe area of the triangle is: "+area+" sq units");
 	}
 
 }
