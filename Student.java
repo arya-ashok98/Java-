95cbb5ac5aeb88d6111047124a582e1215f76fd6
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-28 11:15:22 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+
+	static String name;
+
+	static int roll_no;
+
+	public Student(String name, int roll_no) { //user-defined constructor whose tasks is to assign the values to the variables
+
+		this.name = name;
+
+		this.roll_no = roll_no;
+
+	}
+
+	public static void main(String args[]) {
+
+		Student stud = new Student("John", 2); //calling the user-defined constructor by passing arguments
+
+		System.out.println("\nThe name is "+name+" and the roll number is "+roll_no);
+
+	}
+
+}
+
+		
