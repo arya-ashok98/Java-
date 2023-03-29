08b0b9595d4e752988f02980d5623a2dd6cbcf7f
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-29 13:22:44 +0530

    Add files via upload

---
+++ DisplayArray.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class DisplayArray {
+
+	public static void main(String args[]) {
+
+		int n, i;
+
+		Scanner sc = new Scanner(System.in);
+
+            System.out.println("\nEnter the array limit: ");
+
+            n = sc.nextInt();
+
+		int[] array = new int[n]; // or int array[] = new int[n];
+
+		System.out.println("\nEnter the array elements: ");
+
+            for(i = 0; i < n; i++) {
+
+			array[i] = sc.nextInt();
+
+            }
+
+		System.out.println("\nThe array elements are: ");
+		
+		for(i = 0; i < n; i++) {
+
+			System.out.println(array[i]);
+
+		}
+
+
+	}
+
+}
