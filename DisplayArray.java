dccd1a3f824c01019913379c93f57ab8a63de31c
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-03-29 13:23:27 +0530

    Update DisplayArray.java

--- DisplayArray.java
+++ DisplayArray.java
@@ -8,19 +8,19 @@ public class DisplayArray {
 
 		Scanner sc = new Scanner(System.in);
 
-            System.out.println("\nEnter the array limit: ");
+            	System.out.println("\nEnter the array limit: ");
 
-            n = sc.nextInt();
+           	n = sc.nextInt();
 
 		int[] array = new int[n]; // or int array[] = new int[n];
 
 		System.out.println("\nEnter the array elements: ");
 
-            for(i = 0; i < n; i++) {
+            	for(i = 0; i < n; i++) {
 
 			array[i] = sc.nextInt();
 
-            }
+            	}
 
 		System.out.println("\nThe array elements are: ");
 		
@@ -29,7 +29,6 @@ public class DisplayArray {
 			System.out.println(array[i]);
 
 		}
-
 
 	}
 
