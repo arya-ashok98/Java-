3a1a91ac61543cfee3e33758d9914762ad6fbf31
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-04-03 11:36:05 +0530

    Add files via upload

---
+++ PatternAsterick.java
@@ -0,0 +1,21 @@
+public class PatternAsterick {
+
+	public static void main(String[] args) {
+
+		int i, j;
+
+		for(i = 1; i <= 8; i++) {
+
+			for(j = 8; j >= i; j--) {
+
+				System.out.print("*");
+
+			 }
+
+		System.out.println("");
+
+		}
+
+	}
+
+}
